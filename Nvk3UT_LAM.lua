bfc24769e9305e8bfb8fd09bf6e1b9dbd31b5033
Nvk3 <nvk3@nvk3.net>
2025-12-24 18:17:05 +0100

    QSPACING_005_SetEntryDefaults_AllTrackers_20_3_0

--- Nvk3UT_LAM.lua
+++ Nvk3UT_LAM.lua
@@ -162,7 +162,7 @@ local DEFAULT_WINDOW_BARS = {
 
 local DEFAULT_SPACING = {
     category = { indent = 0, spacingAbove = 3, spacingBelow = 6 },
-    entry = { indent = 40, spacingAbove = 3, spacingBelow = 3 },
+    entry = { indent = 20, spacingAbove = 3, spacingBelow = 0 },
     objective = { indent = 60, spacingAbove = 3, spacingBelow = 3, spacingBetween = 1 },
 }
 
